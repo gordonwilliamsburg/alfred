55e666643155e47665f8a0f609a041fcd52293db
gordonwilliamsburg <zarroukinesrine@gmail.com>
2023-10-12 23:33:15 +0200

    delete unnecessary files

--- backend/src/alfred/api/main.py
+++ backend/src/alfred/api/main.py
@@ -2,15 +2,11 @@
 from fastapi import FastAPI
 
 # Import the routers for prediction and predictions
-from .routers import prediction, predictions
+from .routers import topic
 
 # Create a new FastAPI instance
 app = FastAPI()
 
 # Include the router from prediction
 # This connects the routes defined in the prediction module to the FastAPI app
-app.include_router(prediction.router)
-
-# Include the router from predictions
-# This connects the routes defined in the predictions module to the FastAPI app
-app.include_router(predictions.router)
+app.include_router(topic.router)
